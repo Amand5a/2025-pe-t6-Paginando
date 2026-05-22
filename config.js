086ebae8dc174e40d179626fbfd51865c693bbc1
Amanda Noronha <amanda.fym@gmail.com>
2026-05-21 22:06:39 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const DB_URL = "postgres://admin:SuperSecret123@prod-db.internal:5432/app";
+const GITHUB_TOKEN = "ghp_aB3dE5fG7hI9jK1lM3nO5pQ7rS9tU1vW3xY";
+
+module.exports = { AWS_SECRET_ACCESS_KEY, DB_URL, GITHUB_TOKEN };
